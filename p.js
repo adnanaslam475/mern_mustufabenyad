750c87591b5bab9aafb128b617dd673c94d44226
adnanaslam475 <adnanaslam475@gmail.com>
2021-07-26 19:00:59 +0500

    affivv

--- p.js
+++ p.js
@@ -1,11 +1,25 @@
 
 
-function max(arr) {
+function minmax(arr) {
 
-
-    
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
+            console.log(arr[j], arr[j + 1])
+            if (arr[j] > arr[j + 1])
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        }
+    }
+    console.log(arr)
+    return arr;
 }
 
-max([2, 3, 4, 3, 2, 5, 3, 6])
+function possiblevalues(num) {
+    let numnew = 0
+    for (let i = num; i <= num; i--) {
+        numnew = i * (i - 1)
+    }
+    console.log(numnew)
+};
+possiblevalues(6);
 
-console.log(Math.random().toFixed(25).split('.')[1])
+// console.log(Math.random().toFixed(25).split('.')[1])
